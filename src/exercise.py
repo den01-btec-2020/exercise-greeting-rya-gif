09e786083a0be32ecf1f98278804034a2d082fbe
ryagif <21254@bridgend.ac.uk>
2020-10-01 14:51:12 +0000

    tested

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+  message = input ("Ada")
+  print ("Hi Ada")
+  message = input ("Lily")
+  print ("Hi Lily")
+
 
 if __name__ == '__main__':
     main()
